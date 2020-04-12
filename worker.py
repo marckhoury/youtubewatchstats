1b447d42db7cf29deb4a4f493ab0002884bc2444
marckhoury <khoury@eecs.berkeley.edu>
2020-04-12 16:03:40 -0700

    import urllib

--- worker.py
+++ worker.py
@@ -1,11 +1,12 @@
 import os
+import urllib.parse
 
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+url = urllib.parse.urlparse(os.environ.get('REDISCLOUD_URL'))
 redis_conn = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
 if __name__ == '__main__':
